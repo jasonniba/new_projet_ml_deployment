c7ac25f6db26f19d1790d79f433cfaf919714c57
jasonniba <nibajason1@gmail.com>
2026-03-25 02:06:41 +0100

    Add predict endpoint

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class InputData(BaseModel):
+    feature1: float
+    feature2: float
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+@app.post("/predict")
+def predict(data: InputData):
+    # Fake modèle pour test
+    result = data.feature1 + data.feature2
+    return {"prediction": result}
